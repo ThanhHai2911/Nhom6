f55f271d5c384db799063a7fddb6f2d5c2bc2b10
DuyChili <huynhvanduyk20@gmail.com>
2024-10-11 13:28:46 +0700

    Màn hiình Cài đặt

---
+++ app/src/main/java/com/example/xemphim/activity/CaiDat.java
@@ -0,0 +1,21 @@
+package com.example.xemphim.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.xemphim.R;
+
+public class CaiDat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cai_dat);
+
+    }
+}
